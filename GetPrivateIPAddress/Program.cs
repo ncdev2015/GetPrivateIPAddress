c5a6c870dd80bf907d46547338d5209f169e51e1
ncdev2015 <ncdev2015@gmail.com>
2021-10-21 09:41:41 -0300

    Agregar archivos de proyecto.

---
+++ GetPrivateIPAddress/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+namespace GetPrivateIPAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string IPAddress = GetIPAddress();
+            System.Console.WriteLine(IPAddress);
+            Console.ReadLine();
+        }
+        public static string GetIPAddress()
+        {
+            string IPAddress = string.Empty;
+            IPHostEntry Host = default(IPHostEntry);
+            string Hostname = null;
+            Hostname = System.Environment.MachineName;
+            Host = Dns.GetHostEntry(Hostname);
+            foreach (IPAddress IP in Host.AddressList)
+            {
+                if (IP.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    IPAddress = Convert.ToString(IP);
+                }
+            }
+            return IPAddress;
+        }
+    }
+}
